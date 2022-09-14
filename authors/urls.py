c0d57d00766a2b16e744b4eddcdebfe1cb0ef88e
Gabriel Rocha <gabrielrocha12.gs@gmail.com>
2022-09-14 15:02:39 -0300

    atualização

---
+++ authors/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from . import views
+
+app_name = 'authors'
+
+author_api_router = SimpleRouter()
+author_api_router.register('api', views.AuthorViewSet, basename='author-api')
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('register/create/', views.register_create, name='register_create'),
+    path('login/', views.login_view, name='login'),
+    path('login/create/', views.login_create, name='login_create'),
+    path('logout', views.logout_view, name='logout'),
+]
+
+urlpatterns += author_api_router.urls
