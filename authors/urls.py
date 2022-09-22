617e7a842a3c758b459412c05376a30779193056
Gabriel Rocha <gabrielrocha12.gs@gmail.com>
2022-09-22 13:13:38 -0300

    atualização

--- authors/urls.py
+++ authors/urls.py
@@ -13,9 +13,28 @@ urlpatterns = [
     path('register/create/', views.register_create, name='register_create'),
     path('login/', views.login_view, name='login'),
     path('login/create/', views.login_create, name='login_create'),
-    path('logout', views.logout_view, name='logout'),
-    path('dashboard/', views.dashboard_recipe.DashboardRecipe.as_view(),
-         name='dashboard')
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path(
+        'dashboard/recipe/new/',
+        views.DashboardRecipe.as_view(),
+        name='dashboard_recipe_new'
+    ),
+    path(
+        'dashboard/recipe/delete/',
+        views.DashboardRecipeDelete.as_view(),
+        name='dashboard_recipe_delete'
+    ),
+    path(
+        'dashboard/recipe/<int:id>/edit/',
+        views.DashboardRecipe.as_view(),
+        name='dashboard_recipe_edit'
+    ),
+    path(
+        'profile/<int:id>/',
+        views.ProfileView.as_view(),
+        name='profile'
+    ),
 ]
 
 urlpatterns += author_api_router.urls
--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -8,14 +8,11 @@ from .base import RecipeBaseFunctionalTest
 
 
 @pytest.mark.functional_test
-class RecipeHomePageFuncionalTest(RecipeBaseFunctionalTest):
-
-    @patch('recipes.views.PER_PAGE', new=2)
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_message(self):
-        self.make_recipe()
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('No recipes found here', body.text)
+        self.assertIn('No recipes found here 🥲', body.text)
 
     @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_search_input_can_find_correct_recipes(self):
@@ -46,7 +43,7 @@ class RecipeHomePageFuncionalTest(RecipeBaseFunctionalTest):
             self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
         )
 
-    @patch('recipes.views.PER_PAGE', new=2)
+    @patch('recipes.views.site.PER_PAGE', new=2)
     def test_recipe_home_page_pagination(self):
         self.make_recipe_in_batch()
 
